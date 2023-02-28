15a2b0adad12b7eefcbecc963c8caa94d6ecf9a1
Dorian Rivas <dorianrivas33@gmail.com>
2023-02-27 18:48:45 -0600

    problem 05

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,5 +1,7 @@
 function bindToAnArg(func, arg) {
-  // Your code here
+  // const bindToFunc = func.bind(arg)
+  // return bindToFunc;
+  return func.bind(null, arg)
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
